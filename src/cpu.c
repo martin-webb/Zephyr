b5ed4dcc08c1178d4408c7c2d269e5b837a6a1f4
Martin Webb <martindanielwebb@gmail.com>
2012-12-27 21:20:30 +0000

    Removed non-existent "flag" register and rearranged 8-bit registers to indicate the available 16-bit pairings.

--- src/cpu.c
+++ src/cpu.c
@@ -50,18 +50,15 @@
 
 typedef struct {
   uint8_t a;
+  uint8_t f;
   uint8_t b;
   uint8_t c;
   uint8_t d;
   uint8_t e;
-  uint8_t f;
   uint8_t h;
   uint8_t l;
-  
   uint16_t sp;
   uint16_t pc;
-  
-  uint8_t flag;
 } CPURegisters;
 
 typedef enum {
