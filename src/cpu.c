297386bf0b6d4455fa222cec2322f4cabd7c516b
Martin Webb <martindanielwebb@gmail.com>
2012-12-30 14:57:14 +0000

    Implemented 16-bit load LD n, nn.

--- src/cpu.c
+++ src/cpu.c
@@ -821,6 +821,31 @@ int main(int argc, char* argv[]) {
       
       /* 16-Bit Loads ***************************************************************************/
       /* LD n, nn ------------------------------------------------------------------------------*/
+      case 0x01: { // LD BC, nn
+        registers.b = readByte(&m, registers.pc++);
+        registers.c = readByte(&m, registers.pc++);
+        cycles += 12;
+        break;
+      }
+      case 0x11: { // LD DE, nn
+        registers.d = readByte(&m, registers.pc++);
+        registers.e = readByte(&m, registers.pc++);
+        cycles += 12;
+        break;
+      }
+      case 0x21: { // LD HL, nn
+        registers.h = readByte(&m, registers.pc++);
+        registers.l = readByte(&m, registers.pc++);
+        cycles += 12;
+        break;
+      }
+      case 0x31: { // LD SP, nn
+        registers.sp = readWord(&m, registers.pc);
+        registers.pc += 2;
+        cycles += 12;
+        break;
+      }
+      
       /* LD SP, HL -----------------------------------------------------------------------------*/
       /* LD HL, SP + n -------------------------------------------------------------------------*/
       /* LDHL SP, n ----------------------------------------------------------------------------*/
