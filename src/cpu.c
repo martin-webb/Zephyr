e74a85c11323fa8f4406362699722d3292d6c133
Martin Webb <martindanielwebb@gmail.com>
2013-01-07 23:30:56 +0000

    Implemented SRA n instruction.

--- src/cpu.c
+++ src/cpu.c
@@ -245,6 +245,15 @@
   cycles += 8; \
   break;
 
+#define MAKE_SRA_N_OPCODE_IMPL(REGISTER) \
+  registers.f |= (registers.REGISTER & BIT_0) << FLAG_REGISTER_C_BIT_SHIFT; \
+  registers.REGISTER = (registers.REGISTER & BIT_7) | (registers.REGISTER >> 1); \
+  registers.f |= (registers.REGISTER == 0) << FLAG_REGISTER_Z_BIT_SHIFT; \
+  registers.f |= 0 << FLAG_REGISTER_N_BIT_SHIFT; \
+  registers.f |= 0 << FLAG_REGISTER_H_BIT_SHIFT; \
+  cycles += 8; \
+  break;
+
 /************************************************************************************************/
 
 typedef struct {
@@ -2109,6 +2118,39 @@ int main(int argc, char* argv[]) {
           }
           
           /* SRA n -----------------------------------------------------------------------------*/
+          case 0x2F: { // SRA A
+            MAKE_SRA_N_OPCODE_IMPL(a)
+          }
+          case 0x28: { // SRA B
+            MAKE_SRA_N_OPCODE_IMPL(b)
+          }
+          case 0x29: { // SRA C
+            MAKE_SRA_N_OPCODE_IMPL(c)
+          }
+          case 0x2A: { // SRA D
+            MAKE_SRA_N_OPCODE_IMPL(d)
+          }
+          case 0x2B: { // SRA E
+            MAKE_SRA_N_OPCODE_IMPL(e)
+          }
+          case 0x2C: { // SRA H
+            MAKE_SRA_N_OPCODE_IMPL(h)
+          }
+          case 0x2D: { // SRA L
+            MAKE_SRA_N_OPCODE_IMPL(l)
+          }
+          case 0x2E: { // SRA (HL)
+            uint8_t value = readByte(&m, (registers.h << 8) | registers.l);
+            registers.f |= (value & BIT_0) << FLAG_REGISTER_C_BIT_SHIFT;
+            value = (value & BIT_7) | (value >> 1);
+            writeByte(&m, (registers.h << 8) | registers.l, value);
+            registers.f |= (value == 0) << FLAG_REGISTER_Z_BIT_SHIFT;
+            registers.f |= 0 << FLAG_REGISTER_N_BIT_SHIFT;
+            registers.f |= 0 << FLAG_REGISTER_H_BIT_SHIFT;
+            cycles += 16;
+            break;
+          }
+          
           /* SRL n -----------------------------------------------------------------------------*/
           
           /**************************************************************************************/
