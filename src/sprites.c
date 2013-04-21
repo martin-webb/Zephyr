88d79121d1e68bb02aac673956d0c5fa0307aed3
Martin Webb <martindanielwebb@gmail.com>
2013-04-21 23:47:59 +0100

    LCD: Fixed sorting of sprites with different X coordinates.

--- src/sprites.c
+++ src/sprites.c
@@ -6,7 +6,7 @@ int spritesCompareByXPosition(const void* a, const void* b)
   const Sprite* spriteB = (const Sprite*)b;
 
   if (spriteA->xPosition != spriteB->xPosition) {
-    return spriteA->xPosition < spriteB->xPosition;
+    return (spriteA->xPosition < spriteB->xPosition) ? -1 : 1;
   } else {
     return a < b; // Comparing by pointer values here is functionally equivalent to comparing by "table ordering" e.g FE00h - highest, FE04h - next highest, etc.
   }
