879ea548a831f8066b509f12ad2fe1abe5221136
Martin Webb <martindanielwebb@gmail.com>
2015-08-31 13:29:02 +0100

    Fixed comment.

--- src/gameboy.c
+++ src/gameboy.c
@@ -113,7 +113,7 @@ void gbRunNFrames(GameBoy* gameBoy, const int frames)
 
     cpuUpdateIME(cpu);
     cartridgeUpdate(memoryController, baseCyclesExecuted);
-    dmaUpdate(memoryController, cpuCyclesExecuted); // Not using speed adjusted cycles because the divider runs twice as fast in double speed mode
+    dmaUpdate(memoryController, cpuCyclesExecuted); // Not using speed adjusted cycles because the DMA transfer runs twice as fast in double speed mode
     hdmaUpdate(memoryController, baseCyclesExecuted);
     timerUpdateDivider(timerController, cpuCyclesExecuted); // Not using speed adjusted cycles because the divider runs twice as fast in double speed mode
     timerUpdateTimer(timerController, cpuCyclesExecuted); // Not using speed adjusted cycles because the timer runs twice as fast in double speed mode
